d34199a207c8e87c134065314585351e53f4e400
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-03-27 14:44:56 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,19 @@
 
 // execute script when window is loaded
 window.onload = function(){
+	// svg dimension variables
+	var w = 900, h = 500;
+
 	// get the <body> element from the DOM
 	var container = d3.select("body")
 		// put a new svg in the body
 		.append("svg")
+		// assign the width
+		.attr("width", w)
+		// assign the height
+		.attr("height", h)
+		// always assign a class (as the block name) for styling and future selection
+		.attr("class", "container")
+		// assign a background color
+		.style("background-color", "rgba(0,0,0,0.2)");
 };
