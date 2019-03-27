e2fd87a5431709ce253e53b60f514ec3393cdd65
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-03-27 13:13:41 -0400

    draft1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Javascript by Geri Rosenberg, 2019 */
+
+// execute script when window is loaded
+window.onload = function(){
+	// get the <body> element from the DOM
+	var container = d3.select("body")
+		// put a new svg in the body
+		.append("svg")
+};
